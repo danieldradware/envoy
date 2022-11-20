fc7eeaa665435eb1ccff8a06c266643b271f1836
danieldradware <117576776+danieldradware@users.noreply.github.com>
2022-11-20 18:05:52 +0200

    Update authenticator.cc
    Signed-off-by: danieldradware <117576776+danieldradware@users.noreply.github.com>

--- source/extensions/filters/http/jwt_authn/authenticator.cc
+++ source/extensions/filters/http/jwt_authn/authenticator.cc
@@ -316,14 +316,13 @@ void AuthenticatorImpl::doneWithStatus(const Status& status) {
   if(Status::Ok != status) {
     ENVOY_LOG(info, "!@!@ Inside AuthenticatorImpl::doneWithStatus -- in first status::ok");
     //Forward the failed status
-    const auto &provider = jwks_data_->getJwtProvider();
     
-    if (!provider.failed_status_in_metadata().empty()) {
+    if (!jwks_data_->getJwtProvider().failed_status_in_metadata().empty()) {
       ::google::protobuf::Struct failed_status;
       auto &failed_status_fields = *failed_status.mutable_fields();
       failed_status_fields["status"].set_string_value(google::jwt_verify::getStatusString(status));
       ENVOY_LOG(info, "!@!@ Inside AuthenticatorImpl::doneWithStatus -- inner if and status reason is: {}",google::jwt_verify::getStatusString(status));
-      set_extracted_jwt_data_cb_(provider.failed_status_in_metadata(), failed_status);
+      set_extracted_jwt_data_cb_(jwks_data_->getJwtProvider().failed_status_in_metadata(), failed_status);
     }
   }
   // If a request has multiple tokens, all of them must be valid. Otherwise it may have
