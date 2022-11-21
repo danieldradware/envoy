3cb8193d6850072effc473a242af9e0dc0970baf
danieldradware <117576776+danieldradware@users.noreply.github.com>
2022-11-21 12:05:15 +0200

    Update authenticator.cc
    Signed-off-by: danieldradware <117576776+danieldradware@users.noreply.github.com>

--- source/extensions/filters/http/jwt_authn/authenticator.cc
+++ source/extensions/filters/http/jwt_authn/authenticator.cc
@@ -317,7 +317,7 @@ void AuthenticatorImpl::doneWithStatus(const Status& status) {
     ENVOY_LOG(info, "!@!@ Inside AuthenticatorImpl::doneWithStatus -- in first status::ok");
     //Forward the failed status
     
-    if (!jwks_data_->getJwtProvider().failed_status_in_metadata().empty()) {
+    if ((jwks_data_ != nullptr) && !jwks_data_->getJwtProvider().failed_status_in_metadata().empty()) {
       ::google::protobuf::Struct failed_status;
       auto &failed_status_fields = *failed_status.mutable_fields();
       failed_status_fields["status"].set_string_value(google::jwt_verify::getStatusString(status));
