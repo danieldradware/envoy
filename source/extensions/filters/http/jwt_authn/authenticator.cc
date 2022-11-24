e57acf1c767e0a5b9768a06cb3463e2b4104171f
danieldradware <117576776+danieldradware@users.noreply.github.com>
2022-11-24 12:25:14 +0200

    Update authenticator.cc
    Signed-off-by: danieldradware <117576776+danieldradware@users.noreply.github.com>

--- source/extensions/filters/http/jwt_authn/authenticator.cc
+++ source/extensions/filters/http/jwt_authn/authenticator.cc
@@ -319,7 +319,8 @@ void AuthenticatorImpl::doneWithStatus(const Status& status) {
     
     if ((jwks_data_ != nullptr) && !jwks_data_->getJwtProvider().failed_status_in_metadata().empty()) {
       ::google::protobuf::Struct failed_status;
-      failed_status.set_string_value(std::to_string(enumToInt(status)));
+      auto &failed_status_fields = *failed_status.mutable_object();
+      failed_status_fields->mutable_fields()->operator[]("status_code").set_string_value(std::to_string(enumToInt(status)));
       ENVOY_LOG(info, "!@!@ Inside AuthenticatorImpl::doneWithStatus -- inner if and status reason is: {}",google::jwt_verify::getStatusString(status));
       set_extracted_jwt_data_cb_(jwks_data_->getJwtProvider().failed_status_in_metadata(), failed_status);
     }
