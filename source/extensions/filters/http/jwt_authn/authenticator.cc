b52aa7a08c5bc9149c2ae45974a795b8e44c60c0
danieldradware <117576776+danieldradware@users.noreply.github.com>
2022-11-30 08:11:19 +0200

    change envoy log from info to debug level

--- source/extensions/filters/http/jwt_authn/authenticator.cc
+++ source/extensions/filters/http/jwt_authn/authenticator.cc
@@ -319,7 +319,7 @@ void AuthenticatorImpl::doneWithStatus(const Status& status) {
       ProtobufWkt::Struct failed_status;
       auto& failed_status_fields = *failed_status.mutable_fields();
       failed_status_fields["status"].set_string_value(std::to_string(enumToInt(status)));
-      ENVOY_LOG(info, "Writing to metada failure reason: {}",google::jwt_verify::getStatusString(status));
+      ENVOY_LOG(debug, "Writing to metada failure reason: {}",google::jwt_verify::getStatusString(status));
       set_extracted_jwt_data_cb_(jwks_data_->getJwtProvider().failed_status_in_metadata(), failed_status);
     }
   }
